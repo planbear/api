565fbf4a5d11e23dddc30fd9f7f0c1e5ef357604
Ali Zahid <ali.zahid@live.com>
2019-09-18 19:17:50 +0400

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -9,39 +9,35 @@ import auth, { getLocation, getUser } from './auth'
 import resolvers from './resolvers'
 import typeDefs from './schema'
 
-!(async () => {
-  connect(
-    MONGO_URI as string,
-    {
-      useCreateIndex: true,
-      useFindAndModify: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+connect(
+  MONGO_URI as string,
+  {
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
+
+const schema = applyMiddleware(
+  makeExecutableSchema({
+    resolvers,
+    typeDefs
+  }),
+  auth
+)
+
+const server = new ApolloServer({
+  schema,
+  async context({ req }) {
+    const location = getLocation(req)
+    const user = await getUser(req)
+
+    return {
+      location,
+      user
     }
-  )
+  }
+})
 
-  const schema = applyMiddleware(
-    makeExecutableSchema({
-      resolvers,
-      typeDefs
-    }),
-    auth
-  )
-
-  const server = new ApolloServer({
-    schema,
-    async context({ req }) {
-      const location = getLocation(req)
-      const user = await getUser(req)
-
-      return {
-        location,
-        user
-      }
-    }
-  })
-
-  const { url } = await server.listen()
-
-  console.log(`🚀  Server ready at ${url}`)
-})()
+server.listen()
